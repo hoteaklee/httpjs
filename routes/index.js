8e85da6c3d9e86423d252121100ec960b9af3552
hoteaklee <cc18ht@naver.com>
2023-02-15 15:49:15 +0900

    express - static,sendfile,morgan

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
+//index.js
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
-const html = 'text/html; charset=utf-8'
 
 // show index page
 router.get('/',(req,res)=>{
-        res.type(html);
-        res.end('<h1>index 페이지 입니다.</h1>');
+        res.sendFile(path.join(__dirname,'../public', 'index.html'));
 });
+
+
+
+//단순한 그림파일을 화면에 표시하기 위해
+// 일일히 라우팅 설정하는 것은 번거로움
+// router.get('/book.png',(req,res)=>{
+//         res.sendFile(path.join(__dirname,'../static/img', 'book.png'));
+// });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
+//user.js
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 const html = 'text/html; charset=utf-8'
 
 // show user page
 router.get('/',(req,res)=>{
-    res.type(html);
-    res.end('<h1>user 페이지 입니다.</h1>');
+    res.sendFile(path.join(__dirname,'../public', 'user.html'));
 });
 router.get('/add',(req,res) =>{
     res.type(html);
